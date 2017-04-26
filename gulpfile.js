27f7d4b47fc94cdf3830dbe9662cb8b5592c9bf1
darkenvy <darkenvy6@gmail.com>
2017-04-26 12:55:25 -0700

    finished gulp & organization. Now using scss which compiles to css and mnified versions

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp      = require('gulp'),
     concat    = require('gulp-concat'),
     rename    = require('gulp-rename'),
     uglifycss = require('gulp-uglifycss'),
-    sass      = require('gulp-sass');
+    sass      = require('gulp-sass'),
+    gzip = require('gulp-gzip'),
+    size = require('gulp-filesize');
 
-var files = 'css/ek/**/*.css',  
+var files = 'css/ek/**/*.scss',  
     dest = 'dist/';
 
 gulp.task('sass', function (cb) {
@@ -16,14 +18,17 @@ gulp.task('sass', function (cb) {
 
 gulp.task('concat', function(cb) {  
   gulp.src(files)
+    .pipe(sass().on('error', sass.logError))
     .pipe(concat('ek.css'))
     .pipe(gulp.dest(dest + 'css-concat'))
     .pipe(rename('ek.min.css'))
     .pipe(uglifycss({
-      // "maxLineLen": 80,
       "uglyComments": true
     }))
-    .pipe(gulp.dest(dest + 'css-concat'));
+    .pipe(gulp.dest(dest + 'css-concat'))
+    .pipe(gzip())
+    .pipe(gulp.dest(dest + 'css-concat'))
+    .pipe(size());
   return cb();
 });
 
